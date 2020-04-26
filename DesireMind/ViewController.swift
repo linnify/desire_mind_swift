20dca1de4e9a433ad0b3fe7fb171928fdbaa9f83
Vlad Rusu <vlad.rusu@linnify.com>
2020-04-26 12:25:30 +0300

    Initial commit, project setup

---
+++ DesireMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesireMind
+//
+//  Created by Vlad Rusu on 26/04/2020.
+//  Copyright © 2020 Linnify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
